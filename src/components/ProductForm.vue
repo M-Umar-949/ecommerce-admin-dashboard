f5ab3fb360f4e60f8d1d695c18cfbfc50ab028a0
M-Umar-949 <umarrajput930@gmail.com>
2025-05-27 21:49:14 +0500

    Changing api

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -337,7 +337,7 @@ import { ref, reactive } from 'vue'
 const props = defineProps({
   apiUrl: {
     type: String,
-    default: 'http://localhost:5000'
+    default: import.meta.env.VUE_APP_PROD_URL || 'http://localhost:5000'
   }
 })
 
